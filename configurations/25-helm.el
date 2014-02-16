165b7e8f408383df89d242c83dfbf27f2ddef39b
yoshihara <h.yoshihara@everyleaf.com>
2014-02-16 22:36:28 +0900

    Add more helm keybinds

--- configurations/25-helm.el
+++ configurations/25-helm.el
@@ -2,6 +2,7 @@
   (global-set-key (kbd "C-;") 'helm-mini)
   (global-set-key (kbd "M-r") 'helm-resume)
   (global-set-key (kbd "C-x g") 'helm-imenu)
+  (global-set-key (kbd "M-x") 'helm-M-x)
   (global-set-key (kbd "M-y") 'helm-show-kill-ring)
   (global-set-key (kbd "C-x b") 'helm-buffers-list)
 
@@ -11,9 +12,21 @@
     (global-set-key (kbd "C-x C-g") 'helm-git-find-files)
     )
   (when (require 'helm-ls-git nil t)
+    ;;;
     )
+
+  (define-key helm-map (kbd "C-h") 'delete-backward-char)
+  (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
   ;; For find-file etc.
   (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+  (define-key helm-read-file-map (kbd "C-u") 'helm-select-action)
   ;; For helm-find-files etc.
   (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+  (define-key helm-find-files-map (kbd "C-u") 'helm-select-action)
+
+  ;; Emulate `kill-line' in helm minibuffer
+  (setq helm-delete-minibuffer-contents-from-point t)
+  (defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
+    "Emulate `kill-line' in helm minibuffer"
+    (kill-new (buffer-substring (point) (field-end))))
 )
