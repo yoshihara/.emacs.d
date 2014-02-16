aa669bf5a8753cc0aa6d13dff2c1cecada35cac1
yoshihara <h.yoshihara@everyleaf.com>
2014-02-16 18:24:21 +0900

    Add whitespace setting

---
+++ configurations/40-whitespace.el
@@ -0,0 +1,19 @@
+(require 'whitespace)
+
+(setq whitespace-style '(face tabs tab-mark spaces space-mark lines-tail trailing space-before-tab space-after-tab::space))
+(setq whitespace-space-regexp "\\(\x3000+\\)")
+(setq whitespace-display-mappings
+      '((space-mark ?\x3000 [?\　])
+        (tab-mark   ?\t   [?\xBB ?\t])
+        ))
+(global-whitespace-mode t)
+
+(set-face-attribute 'whitespace-trailing nil
+                    :foreground "DeepPink"
+                    :underline t)
+(set-face-attribute 'whitespace-tab nil
+                    :foreground "LightSkyBlue"
+                    :underline t)
+(set-face-attribute 'whitespace-space nil
+                    :foreground "Yellow"
+                    :weight 'bold)
