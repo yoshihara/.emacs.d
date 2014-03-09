bd21dd44f6743958f6bdd88cc67ed588285c238c
yoshihara <h.yoshihara@everyleaf.com>
2014-03-09 16:59:59 +0900

    logalimacs: Use global use ruby for logaling-command

--- configurations/40-logalimacs.el
+++ configurations/40-logalimacs.el
@@ -1,22 +1,11 @@
 (el-get 'sync '(logalimacs))
 
-(defun loga-with-ruby19 (command)
-;; rbenvでlogalimacsを使うときは1.9をつかう
-  (rbenv-use "1.9.3-p429")
-  (funcall command)
-  (rbenv-use-global)
-)
-
 ;;; keybinds
 (when (require 'logalimacs nil t)
-  (global-set-key (kbd "M-g M-i")
-    (lambda () (interactive) (loga-with-ruby19 'loga-interactive-command)))
-  (global-set-key (kbd "M-g M-l")
-    (lambda () (interactive) (loga-with-ruby19 'loga-lookup-at-manually)))
-  (global-set-key (kbd "M-g M-a")
-    (lambda () (interactive) (loga-with-ruby19 'loga-add)))
-  (global-set-key (kbd "C-:")
-    (lambda () (interactive) (loga-with-ruby19 'loga-lookup-in-popup))))
+  (global-set-key (kbd "M-g M-i") 'loga-interactive-command)
+  (global-set-key (kbd "M-g M-l") 'loga-lookup-at-manually)
+  (global-set-key (kbd "M-g M-a") 'loga-add)
+  (global-set-key (kbd "C-:")     'loga-lookup-in-popup))
 
 ;;; 以下はお好みで設定してください(version1.0から)
 ;; lookupに--dictionaryオプションを利用する場合
