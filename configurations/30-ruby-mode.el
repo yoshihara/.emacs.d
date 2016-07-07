df92dc2497f0dc778581b501345b455e9d5daaae
yoshihara <h.yoshihara@everyleaf.com>
2016-07-07 22:51:45 +0900

    Add ruby-insert-end func

--- configurations/30-ruby-mode.el
+++ configurations/30-ruby-mode.el
@@ -1,6 +1,12 @@
 (el-get 'sync '(ruby-mode))
 (require 'ruby-mode)
 
+(defun ruby-insert-end ()
+  "Insert 'end'."
+  (interactive)
+  (insert "end")
+  (ruby-indent-line))
+
 (add-hook 'ruby-mode-hook
           (lambda ()
             ;; C-M-pとC-M-nをすでに
@@ -10,7 +16,8 @@
             ;; 上書きしてしまうので無効にする。
             ;; 2012-07-11
             (define-key ruby-mode-map (kbd "C-M-p") nil)
-            (define-key ruby-mode-map (kbd "C-M-n") nil)))
+            (define-key ruby-mode-map (kbd "C-M-n") nil)
+            (define-key ruby-mode-map (kbd "C-C C-e") 'ruby-insert-end)))
 
 (defun ruby-mode-set-encoding ())
 
