11e09ce0202695cd03552227e2f1e50b92853cd1
yoshihara <h.yoshihara@everyleaf.com>
2016-07-07 22:49:58 +0900

    Follow current version magit

--- configurations/30-magit.el
+++ configurations/30-magit.el
@@ -1,10 +1,15 @@
 (require 'magit)
+
 (defun magit-setup-diff ()
   ;; diffを表示しているときに文字単位での変更箇所も強調表示する
   ;; 'allではなくtにすると現在選択中のhunkのみ強調表示する
-  (setq magit-diff-refine-hunk 'all)
-  ;; diff用のfaceを設定する
-  (diff-mode-setup-faces)
-  ;; diffの表示設定が上書きされてしまうのでハイライトを無効にする
-  (set-face-attribute 'magit-item-highlight nil :inherit nil))
+  ;; 2012-04-02
+  (setq magit-diff-refine-hunk 'all))
 (add-hook 'magit-mode-hook 'magit-setup-diff)
+
+(defun magit-setup-push ()
+  ;; pushするときに確認しない
+  ;; pushするときは現在のブランチのみをpushする
+  ;; 2015-08-17
+  (setq magit-push-always-verify nil))
+(add-hook 'magit-mode-hook 'magit-setup-push)
