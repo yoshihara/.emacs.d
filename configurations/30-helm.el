55a83cd5197c4b4ad0ad78e0505deb1cf4725629
yoshihara <h.yoshihara@everyleaf.com>
2016-07-07 22:47:48 +0900

    Add key-bind with helm

--- configurations/30-helm.el
+++ configurations/30-helm.el
@@ -5,6 +5,7 @@
   (global-set-key (kbd "M-x") 'helm-M-x)
   (global-set-key (kbd "M-y") 'helm-show-kill-ring)
   (global-set-key (kbd "C-x b") 'helm-buffers-list)
+  (global-set-key (kbd "C-x C-f") 'helm-find-files)
 
   (helm-mode 1)
 
