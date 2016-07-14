256854af4f24ff4bb506b42bf4e6b71f7f966237
yoshihara <h.yoshihara@everyleaf.com>
2016-07-15 01:30:06 +0900

    Remove each needless package configuration

--- configurations/30-js-mode.el
+++
@@ -1,7 +0,0 @@
-(el-get 'sync '(mooz-js2-mode))
-
-(add-hook 'js2-mode-hook
-          (lambda ()
-            ;; ASTを使わないで移動するS式を求める。
-            ;; 2012-08-14
-            (setq forward-sexp-function nil)))
