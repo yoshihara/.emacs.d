a61d20686b04edfe31dfdf81e46ce8e75ee277fb
yoshihara <yoshihara@users.noreply.github.com>
2017-08-15 15:54:58 +0900

    Use 2 space indent for js2-mode

--- configurations/30-js2-mode.el
+++ configurations/30-js2-mode.el
@@ -1,5 +1,9 @@
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-
+(add-hook 'js2-mode-hook
+          #'(lambda ()
+              (setq js2-basic-offset 2
+                    indent-tabs-mode nil)
+              ))
 (setq js2-strict-missing-semi-warning nil)
 
