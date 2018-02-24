1c646bb0416d72cbcedaef6333b019208a271f4c
yoshihara <yoshihara@users.noreply.github.com>
2018-02-25 03:44:21 +0900

    Use js2-jsx-mode for .jsx

--- configurations/30-js2-mode.el
+++ configurations/30-js2-mode.el
@@ -1,6 +1,7 @@
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.es6$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . js2-jsx-mode))
 
 (add-hook 'js2-mode-hook
           #'(lambda ()
