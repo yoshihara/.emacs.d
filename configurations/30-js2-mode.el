b315ec10703aa92898cb82c3a953e77b7b87a18a
yoshihara <yoshihara@users.noreply.github.com>
2018-02-25 03:44:10 +0900

    Use js2-mode for .es6

--- configurations/30-js2-mode.el
+++ configurations/30-js2-mode.el
@@ -1,5 +1,7 @@
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.es6$" . js2-mode))
+
 (add-hook 'js2-mode-hook
           #'(lambda ()
               (setq js2-basic-offset 2
