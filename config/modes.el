9b133da7788f583f27a8dbd207051a6a308560fa
yoshihara <594134+yoshihara@users.noreply.github.com>
2018-11-16 00:17:53 +0900

    Config whitespace-mode in not init-loader' file

--- config/modes.el
+++ config/modes.el
@@ -171,4 +171,25 @@
 
 (defun ruby-mode-set-encoding ())
 
+;; whitespace-mode
+
+(require 'whitespace)
+(setq whitespace-style '(face tabs tab-mark spaces space-mark lines-tail trailing space-before-tab space-after-tab::space))
+(setq whitespace-space-regexp "\\(\x3000+\\)")
+(setq whitespace-display-mappings
+      '((space-mark ?\x3000 [?\　])
+        (tab-mark   ?\t   [?\xBB ?\t])
+        ))
+(global-whitespace-mode t)
+
+(set-face-attribute 'whitespace-trailing nil
+                    :foreground "DeepPink"
+                    :underline t)
+(set-face-attribute 'whitespace-tab nil
+                    :foreground "LightSkyBlue"
+                    :underline nil)
+(set-face-attribute 'whitespace-space nil
+                    :foreground "Yellow"
+                    :weight 'bold)
+
 (provide 'modes)
