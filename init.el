4708649f5ed90e91a96aa36fb782061474d26c19
yoshihara <594134+yoshihara@users.noreply.github.com>
2018-11-16 00:40:19 +0900

    Integrate el-get configuration to init.el

--- init.el
+++ init.el
@@ -10,6 +10,24 @@
     (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
 
+;; el-get
+;; el-getの設定
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path
+             "~/.emacs.d/el-get/recipes")
+(add-to-list 'el-get-recipe-path
+             "~/.emacs.d/elisp/el-get-local-recipes")
+
+(setq el-get-recipe-path (reverse el-get-recipe-path))
+
 ;; init-loader
 (require 'init-loader)
 (setq init-loader-show-log-after-init t)
