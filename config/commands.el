081c84cdb9c2c82825abc9966c45eacf3c40d6fa
yoshihara <594134+yoshihara@users.noreply.github.com>
2018-11-16 00:11:39 +0900

    Move functions closed to use it

--- config/commands.el
+++ config/commands.el
@@ -1,5 +1,17 @@
 ;;; バージョン管理システム
 ;; diredから適切なバージョン管理システムの*-statusを起動
+(defun find-path-in-parents (directory base-names)
+  (or (find-if 'file-exists-p
+               (mapcar (lambda (base-name)
+                         (concat directory base-name))
+                       base-names))
+      (if (string= directory "/")
+          nil
+        (let ((parent-directory (substring directory 0 -1)))
+          (find-path-in-parents parent-directory base-names)))))
+
+(setq this-file-path (file-name-directory (or load-file-name (buffer-file-name))))
+
 (defun dired-vc-status (&rest args)
   (interactive)
   (let ((path (find-path-in-parents (dired-current-directory)
--- configurations/10-utilities.el
+++ configurations/10-utilities.el
@@ -1,14 +1,2 @@
 (defun string-strip (string)
   (replace-regexp-in-string "\\`[ \r\n]*\\|[ \r\n]*\\'" "" string))
-
-(defun find-path-in-parents (directory base-names)
-  (or (find-if 'file-exists-p
-               (mapcar (lambda (base-name)
-                         (concat directory base-name))
-                       base-names))
-      (if (string= directory "/")
-          nil
-        (let ((parent-directory (substring directory 0 -1)))
-          (find-path-in-parents parent-directory base-names)))))
-
-(setq this-file-path (file-name-directory (or load-file-name (buffer-file-name))))
