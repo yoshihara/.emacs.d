d228d15a2708e4dd226ebe099d716e1d137363c7
yoshihara <594134+yoshihara@users.noreply.github.com>
2018-11-16 00:09:39 +0900

    Define "V" for vcs in commands.el

--- config/commands.el
+++ config/commands.el
@@ -1,3 +1,18 @@
+;;; バージョン管理システム
+;; diredから適切なバージョン管理システムの*-statusを起動
+(defun dired-vc-status (&rest args)
+  (interactive)
+  (let ((path (find-path-in-parents (dired-current-directory)
+                                    '(".svn" ".git"))))
+    (cond ((null path)
+           (message "not version controlled."))
+          ((string-match-p "\\.svn$" path)
+           (svn-status (file-name-directory path)))
+          ((string-match-p "\\.git$" path)
+           (magit-status (file-name-directory path))))))
+(define-key dired-mode-map "V" 'dired-vc-status)
+
+;; git-grep
 (defun chomp (str)
   (replace-regexp-in-string "[\n\r]+$" "" str))
 
