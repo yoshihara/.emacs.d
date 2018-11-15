9d67aacbf2895bcf4dca0c48950209a5d4282c52
yoshihara <594134+yoshihara@users.noreply.github.com>
2018-11-15 21:43:39 +0900

    custom-set-variables generates automatically :(

--- configurations/30-js2-mode.el
+++ configurations/30-js2-mode.el
@@ -1,13 +1,5 @@
 (el-get 'sync '(js2-mode))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(js-indent-level 2)
-)
-
 (autoload 'js2-mode "js2" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.es6$" . js2-mode))
