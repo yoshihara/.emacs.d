09aaf3657fb919dc3081bed6d93edbd0c5637123
yoshihara <594134+yoshihara@users.noreply.github.com>
2018-11-16 01:21:33 +0900

    Split configurations for package managers to file

--- init.el
+++ init.el
@@ -49,36 +49,6 @@
 (setq-default indent-tabs-mode nil)
 (electric-indent-mode -1)
 
-;; package.elの設定
-;; https://github.com/melpa/melpa/blob/b9ff4284d6e311960a79a3d20fb0542486c7f67b/README.md
-(require 'package)
-
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
-(package-initialize)
-
-;; el-get
-;; el-getの設定
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-(add-to-list 'el-get-recipe-path
-             "~/.emacs.d/el-get/recipes")
-(add-to-list 'el-get-recipe-path
-             "~/.emacs.d/elisp/el-get-local-recipes")
-
-(setq el-get-recipe-path (reverse el-get-recipe-path))
-
 ;; emacsセットアップ用の設定
 (setq load-path (append
                  '("~/.emacs.d/setup") ;; emacsセットアップ用ファイル入れ（この後設定で読み込むファイルが依存するものはこっち）
@@ -98,8 +68,7 @@
 
 ;; パッケージインストール＆それらを使った定義など
 
-;; パッケージ情報を更新（インストールは別でやる）
-(package-refresh-contents)
+(require 'package-managers)
 
 (require 'modes)
 (require 'other-packages)
