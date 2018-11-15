ae202a167141ee8d9504949c935b05acd57b7827
yoshihara <594134+yoshihara@users.noreply.github.com>
2018-11-15 23:04:17 +0900

    Integrate truncate-line.el to commands.el

--- config/commands.el
+++ config/commands.el
@@ -34,4 +34,15 @@
                  command-args)))
     (grep command)))
 
+;; truncate-line
+(defun toggle-truncate-lines ()
+  (interactive)
+  (if truncate-lines
+      (setq truncate-lines nil)
+    (setq truncate-lines t))
+  (recenter))
+
+(global-set-key "\C-c\C-l" 'toggle-truncate-lines)
+
+
 (provide 'commands)
--- configurations/70-truncate-line.el
+++
@@ -1,8 +0,0 @@
-(defun toggle-truncate-lines ()
-  (interactive)
-  (if truncate-lines
-      (setq truncate-lines nil)
-    (setq truncate-lines t))
-  (recenter))
-
-(global-set-key "\C-c\C-l" 'toggle-truncate-lines)
