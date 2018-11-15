1c89f8d0dfb3091474aa8d5327c2a144f22e71a0
yoshihara <594134+yoshihara@users.noreply.github.com>
2018-11-16 00:08:11 +0900

    Fix indent

--- config/other-packages.el
+++ config/other-packages.el
@@ -71,7 +71,7 @@
   (defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
     "Emulate `kill-line' in helm minibuffer"
     (kill-new (buffer-substring (point) (field-end))))
-)
+  )
 
 ;; anzu
 (ensure-installed-package 'anzu)
