23fefa84bc8000f1759e1e573ef7478e595426dc
yoshihara <594134+yoshihara@users.noreply.github.com>
2018-11-15 23:34:19 +0900

    Install js2-mode in not init-loader' file

--- config/modes.el
+++ config/modes.el
@@ -126,4 +126,20 @@
    ((t (:foreground "#FF7F00"))))                          ; cssのタグ
 )
 
+;; js2-mode
+(el-get 'sync '(js2-mode))
+(autoload 'js2-mode "js2" nil t)
+(el-get-init 'js2-mode)
+
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.es6$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . js2-jsx-mode))
+
+(add-hook 'js2-mode-hook
+          #'(lambda ()
+              (setq js2-basic-offset 2
+                    indent-tabs-mode nil)
+              ))
+(setq js2-strict-missing-semi-warning nil)
+
 (provide 'modes)
