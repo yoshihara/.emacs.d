daa188e64000f6b5116f1f1c6a5d999c404cbec2
yoshihara <594134+yoshihara@users.noreply.github.com>
2018-11-15 23:35:04 +0900

    Install ruby-mode in not init-loader' file

--- config/modes.el
+++ config/modes.el
@@ -142,4 +142,28 @@
               ))
 (setq js2-strict-missing-semi-warning nil)
 
+;; ruby-mode
+;; el-getから元々入れていたが、「invalid syntax "#"」というエラーが出るので一旦ビルドインで試す
+(require 'ruby-mode)
+
+(defun ruby-insert-end ()
+  "Insert 'end'."
+  (interactive)
+  (insert "end")
+  (ruby-indent-line))
+
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            ;; C-M-pとC-M-nをすでに
+            ;; previous-multiframe-windowとnext-multiframe-windowに
+            ;; 割り当てているが
+            ;; ruby-beginning-of-blockとruby-end-of-blockが
+            ;; 上書きしてしまうので無効にする。
+            ;; 2012-07-11
+            (define-key ruby-mode-map (kbd "C-M-p") nil)
+            (define-key ruby-mode-map (kbd "C-M-n") nil)
+            (define-key ruby-mode-map (kbd "C-C C-e") 'ruby-insert-end)))
+
+(defun ruby-mode-set-encoding ())
+
 (provide 'modes)
